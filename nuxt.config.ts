69233e8ba9bd128aa8368d15991107b00ce54846
kyxtya023 <kyxtya023@yandex.ru>
2024-12-08 21:27:33 +0300

    Initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
     "nuxt-swiper"
   ],
   nitro: {
-    preset: 'node-server', // Или другое, если необходимо
+    preset: 'vercel-edge', // Или другое, если необходимо
     routeRules: {
       '/api/**': {
         headers: {
